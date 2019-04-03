191d160b8d790b24c354838e95cdc2d69aaf39d9
Oleksii Sachek <alexeycacheknew@gmail.com>
2019-04-03 15:08:37 +0300

    Add unit tests

---
+++ src/sTextEditor.Tests/Services/CompressionServiceTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using NUnit.Framework;
+using sTextEditor.Services;
+
+namespace sTextEditor.Tests.Services
+{
+    [TestFixture]
+    public class CompressionServiceTest
+    {
+        [Test]
+        [TestCaseSource(typeof(CompressionServiceDataFactory), "SampleSmallRandomData")]
+        public void CompressAndDecompressToSameResultTest(byte[] data)
+        {
+            var compressionService = new CompressionService();
+            var compressedData = compressionService.Compress(data);
+            var decompressedData = compressionService.Decompress(compressedData);
+
+            Assert.True(data.SequenceEqual(decompressedData));
+        }
+
+        [Test]
+        [TestCaseSource(typeof(CompressionServiceDataFactory), "SampleLargeRandomData")]
+        public void CompressedDataSmallerThenOriginalTest(byte[] data)
+        {
+            var compressionService = new CompressionService();
+            var compressedData = compressionService.Compress(data);
+
+            var originalDataSize = data.Length * sizeof(byte);
+            var compressedDataSize = compressedData.Length * sizeof(byte);
+
+            Assert.LessOrEqual(compressedDataSize, compressedDataSize,
+                "Compressed data size ({0}) is greater then original ({1}).",
+                compressedDataSize, originalDataSize);
+        }
+    }
+
+    public class CompressionServiceDataFactory
+    {
+        private static Random random = new Random();
+
+        public static IEnumerable SampleSmallRandomData
+        {
+            get
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    yield return GenerateRandomBytes(100);
+                }
+            }
+        }
+
+        public static IEnumerable SampleLargeRandomData
+        {
+            get
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    yield return GenerateRandomBytes(10000);
+                }
+            }
+        }
+
+        private static byte[] GenerateRandomBytes(int size)
+        {
+            var data = new byte[size];
+            random.NextBytes(data);
+            return data;
+        }
+    }
+}
---
+++ src/sTextEditor.Tests/Services/FormattingServiceTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using NUnit.Framework;
+using sTextEditor.Services;
+
+namespace sTextEditor.Tests.Services
+{
+    [TestFixture]
+    public class FormattingServiceTest
+    {
+        [Test]
+        [TestCaseSource(typeof(FormattingServiceDataFactory), "SampleJsonData")]
+        public void CorrectJsonFormattingTest(string unformattedText, string formattedText)
+        {
+            var formattingService = new FormattingService();
+            var serviceFormattedText = formattingService.FormatAsJson(unformattedText);
+
+            Assert.AreEqual(serviceFormattedText, formattedText);
+        }
+
+        [Test]
+        [TestCaseSource(typeof(FormattingServiceDataFactory), "SampleXmlData")]
+        public void CorrectXmlFormattingTest(string unformattedText, string formattedText)
+        {
+            var formattingService = new FormattingService();
+            var serviceFormattedText = formattingService.FormatAsXml(unformattedText);
+
+            Assert.AreEqual(serviceFormattedText, formattedText);
+        }
+    }
+
+    public class FormattingServiceDataFactory
+    {
+        public static IEnumerable SampleJsonData
+        {
+            get
+            {
+                var currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+                yield return new TestCaseData(
+                    File.ReadAllText(Path.Combine(currentDirectory, "SampleData/unformatted.json")), 
+                    File.ReadAllText(Path.Combine(currentDirectory, "SampleData/formatted.json")));
+            }
+        }
+
+        public static IEnumerable SampleXmlData
+        {
+            get
+            {
+                var currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+                yield return new TestCaseData(
+                    File.ReadAllText(Path.Combine(currentDirectory, "SampleData/unformatted.xml")),
+                    File.ReadAllText(Path.Combine(currentDirectory, "SampleData/formatted.xml")));
+            }
+        }
+    }
+}
